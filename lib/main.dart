0a9969175ebefdf136d9c1b8751d19e644d15d99
PhalugunaRao <phalu900@gmail.com>
2020-05-09 22:44:19 +0530

    Memory Efficient dynamic ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import "package:flutter/material.dart";
 import 'app_screens/first_screen.dart';
 import 'app_screens/home.dart';
@@ -11,7 +12,7 @@ void main() {
     title: "Exploring UI widgets",
     home: Scaffold(
       appBar: AppBar(title: Text("Basic List View"),),
-      body: getListView(),
+      body: getListViewData(),
     ),
     //home: RowsAndColum(),
   ));
@@ -55,6 +56,27 @@ Widget getListView(){
       Container(color: Colors.red, height: 50.0,)
 
     ],
+  );
+  return listView;
+}
+
+List<String> getListElements(){
+  var items = List<String>.generate(300, (counter) => "Item $counter");
+  return items;
+}
+
+Widget getListViewData(){
+  var listItems =getListElements();
+  var listView =ListView.builder(
+      itemBuilder: (context,index){
+      return ListTile(
+        leading: Icon(Icons.arrow_right),
+        title: Text(listItems[index]),
+        onTap: (){
+          debugPrint('${listItems[index]} was tapped');
+        },
+      );
+   }
   );
   return listView;
 }
