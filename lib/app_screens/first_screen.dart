842ed7b5f1f9f74398761002b91e47f46a38a83a
PhalugunaRao <phalu900@gmail.com>
2020-05-09 14:40:00 +0530

    Add logic

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,17 +1,26 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.lightBlueAccent,
       child: Center(child: Text(
-        "Hello Geethika",
+        "Geethika your lucky number is ${generateLuckNumber()}",
         textDirection: TextDirection.ltr,
         style: TextStyle(color: Colors.white,fontSize: 40.0),
       ) ,
       ),
     );
+  }
+
+  int generateLuckNumber(){
+    var random = Random();
+    int luckyNumber = random.nextInt(10);
+    return luckyNumber;
   }
 
 }
