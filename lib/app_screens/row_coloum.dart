50f761a1a260121916db0e948fdc7e3453ee78bd
PhalugunaRao <phalu900@gmail.com>
2020-05-09 21:50:37 +0530

    Raised Button and Alert Dialog Widget

--- lib/app_screens/row_coloum.dart
+++ lib/app_screens/row_coloum.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
-
-class RowsAndColum extends StatelessWidget{
+class RowsAndColum extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-        child:  Container(
-          padding: EdgeInsets.only(left: 10.0,top: 70.0),
-          alignment: Alignment.center,
-          color: Colors.deepPurple,
+        child: Container(
+            padding: EdgeInsets.only(left: 10.0, top: 70.0),
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
             child: Column(
               children: <Widget>[
                 Row(
                   children: <Widget>[
-                    Expanded(child: Text(
+                    Expanded(
+                        child: Text(
                       "Flight",
                       textDirection: TextDirection.ltr,
                       style: TextStyle(
@@ -22,10 +22,10 @@ class RowsAndColum extends StatelessWidget{
                           fontSize: 30.0,
                           fontFamily: 'Raleway',
                           fontWeight: FontWeight.w300,
-                          color: Colors.white
-                      ),
+                          color: Colors.white),
                     )),
-                    Expanded(child: Text(
+                    Expanded(
+                        child: Text(
                       "Flight Demo Samples from mumbai to delhi via kolakat",
                       textDirection: TextDirection.ltr,
                       style: TextStyle(
@@ -33,15 +33,14 @@ class RowsAndColum extends StatelessWidget{
                           fontSize: 20.0,
                           fontFamily: 'Raleway',
                           fontWeight: FontWeight.w300,
-                          color: Colors.white
-                      ),
-                    )
-                    ),
+                          color: Colors.white),
+                    )),
                   ],
                 ),
                 Row(
                   children: <Widget>[
-                    Expanded(child: Text(
+                    Expanded(
+                        child: Text(
                       "Flight",
                       textDirection: TextDirection.ltr,
                       style: TextStyle(
@@ -49,10 +48,10 @@ class RowsAndColum extends StatelessWidget{
                           fontSize: 30.0,
                           fontFamily: 'Raleway',
                           fontWeight: FontWeight.w300,
-                          color: Colors.white
-                      ),
+                          color: Colors.white),
                     )),
-                    Expanded(child: Text(
+                    Expanded(
+                        child: Text(
                       "Flight Demo Samples from mumbai to delhi via kolakat",
                       textDirection: TextDirection.ltr,
                       style: TextStyle(
@@ -60,20 +59,16 @@ class RowsAndColum extends StatelessWidget{
                           fontSize: 20.0,
                           fontFamily: 'Raleway',
                           fontWeight: FontWeight.w300,
-                          color: Colors.white
-                      ),
-                    )
-                    ),
+                          color: Colors.white),
+                    )),
                   ],
                 ),
-                FlightImageAsset()
-
+                FlightImageAsset(),
+                FlightBookingButton()
               ],
             )
 
-
-
-          /*child: Row(
+            /*child: Row(
             children: <Widget>[
              Expanded(child: Text(
                 "Flight",
@@ -102,18 +97,47 @@ class RowsAndColum extends StatelessWidget{
           )
 */
 
-        )
-    );
+            ));
   }
-  
 }
 
-class FlightImageAsset extends StatelessWidget{
+class FlightImageAsset extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AssetImage assetImage = AssetImage('images/aarogya_setu_icon.png');
-    Image image = Image(image:assetImage, width: 200.0,height: 200.0);
+    Image image = Image(image: assetImage, width: 200.0, height: 200.0);
     return Container(child: image);
   }
+}
 
+class FlightBookingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+          color: Colors.deepOrange,
+          child: Text(
+            "Book Flight",
+            style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.white,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w700),
+          ),
+          elevation: 6.0,
+          onPressed: () => bookTickets(context)),
+    );
+  }
+
+  void bookTickets(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Flight Booked Successfully"),
+      content: Text("Have a pleasant flight Demo"),
+    );
+
+    showDialog(context: context, builder: (BuildContext contex) => alertDialog);
+  }
 }
