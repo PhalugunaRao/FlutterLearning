d99f034883b729a42971c5db8b778cbef0c0c16f
PhalugunaRao <phalu900@gmail.com>
2020-05-09 12:31:28 +0530

    first commit

---
+++ android/app/src/main/kotlin/smartherd/com/MainActivity.kt
@@ -0,0 +1,6 @@
+package smartherd.com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+void main(){
+  runApp(
+    Center(child: Text(
+        "Hello Geethika",
+        textDirection: TextDirection.ltr
+      ) ,
+    )
+  );
+}
