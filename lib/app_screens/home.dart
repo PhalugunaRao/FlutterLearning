cc902c469b58aab96b378150fbceb80ca0d4cbb9
PhalugunaRao <phalu900@gmail.com>
2020-05-17 21:54:44 +0530

    basic widgets

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,32 +1,100 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
 
+class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-
-  return Center(
-    child:  Container(
+    return Center(
+      child: Container(
       alignment: Alignment.center,
-      color: Colors.deepPurple,
-      //width:200.0,
-     // height :100.0,
-     // margin: EdgeInsets.only(left:40.0,top: 20.0),
-      //padding: EdgeInsets.all(20.0),
-      child: Text(
-          "Flight",
-          textDirection: TextDirection.ltr,
-        style: TextStyle(
-           decoration: TextDecoration.none,
-            fontSize: 50.0,
-            fontFamily: 'Raleway',
-            fontWeight: FontWeight.w300,
-            fontStyle: FontStyle.italic
-        ),
-      ),
-    )
-  );
+      color: Colors.deepOrangeAccent,
+      padding: EdgeInsets.only(top: 30.0,left: 10.0),
+      child:Column(
+        children: <Widget>[
+          Row(children: <Widget>[
+            Expanded(child:Text(
+              "Demo Data",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  fontSize: 30.0,
+                  decoration: TextDecoration.none,
+                  fontFamily: 'Roboto'
+              ),)),
+            Expanded (child:Text(
+              "Anitha Sample Text",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  fontSize: 20.0,
+                  decoration: TextDecoration.none,
+                  fontFamily: 'Roboto'
+              ),)),
+          ],),Row(children: <Widget>[
+            Expanded(child:Text(
+              "Geethika",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  fontSize: 30.0,
+                  decoration: TextDecoration.none,
+                  fontFamily: 'Roboto'
+              ),)),
+            Expanded (child:Text(
+              "Anitha Sample Text",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  fontSize: 20.0,
+                  decoration: TextDecoration.none,
+                  fontFamily: 'Roboto'
+              ),)),
+          ],),
+          PizzImage(),
+          OrderButton(),
+        ],
+
+      )
+    ));
   }
+}
+
+class PizzImage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizz =AssetImage('assets/images/aarogya_setu_icon.png');
+    Image img =Image(image: pizz,width: 150.0,height: 150.0,);
+    return Container(child: img);
+  }
+
+}
+
+class OrderButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top:30.0),
+      child:RaisedButton(
+        child:Text("Order Your Pizza!"),
+        color: Colors.lightGreen,
+        elevation: 5.0,
+        onPressed: (){
+        order(context);
+        },
+
+      )
+    );
+   return button;
+  }
+
+  void order(BuildContext context){
+    var alert =AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thankq for your order"),
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return alert;
+      }
+    );
+
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,10 @@
-import "package:flutter/material.dart";
-import 'app_screens/first_screen.dart';
-import 'app_screens/home.dart';
-import 'app_screens/row_coloum.dart';
+import 'package:flutter/material.dart';
+import './app_screens/home.dart';
 
-//void main() => runApp(new MyFlutterApp());
+void main(){
+  runApp(new MaterialApp(
+    title: "Simple Layouts",
+    home: new Home(),
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Exploring UI widgets",
-    home: Scaffold(
-      appBar: AppBar(title: Text("Basic List View"),),
-      body: getListViewData(),
-      floatingActionButton: FloatingActionButton(
-        onPressed :(){
-          debugPrint("FAB CLICK");
-        },
-        child: Icon(Icons.add),
-        tooltip: 'Add One More Item',
-      ),
-    ),
-    //home: RowsAndColum(),
   ));
 }
-
-void showSnackBar( BuildContext context, String  item){
-  var snackBar = SnackBar(
-    content: Text("you just tapped $item"),
-    action: SnackBarAction(
-      label: "UNDO",
-      onPressed: (){
-        debugPrint("on click undo");
-      },
-    ),
-  );
-  Scaffold.of(context).showSnackBar(snackBar);
-}
-
-class MyFlutterApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("Geethika App"),
-            ),
-            body: FirstScreen()));
-  }
-}
-
-Widget getListView(){
-  var listView = ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful View !"),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: () => debugPrint("Landscape tapped"),
-      ),
-      ListTile(
-        leading: Icon(Icons.computer),
-        title: Text("Landscape"),
-      ),ListTile(
-        leading: Icon(Icons.battery_alert),
-        title: Text("Landscape"),
-      ),ListTile(
-        leading: Icon(Icons.phone),
-        title: Text("Landscape"),
-      ),
-      Text("Yet another element in List"),
-      Container(color: Colors.red, height: 50.0,)
-
-    ],
-  );
-  return listView;
-}
-
-List<String> getListElements(){
-  var items = List<String>.generate(300, (counter) => "Item $counter");
-  return items;
-}
-
-Widget getListViewData(){
-  var listItems =getListElements();
-  var listView =ListView.builder(
-      itemBuilder: (context,index){
-      return ListTile(
-        leading: Icon(Icons.arrow_right),
-        title: Text(listItems[index]),
-        onTap: (){
-          showSnackBar(context,listItems[index]);
-          debugPrint('${listItems[index]} was tapped');
-        },
-      );
-   }
-  );
-  return listView;
-}
-
